fca68072849e5d96e80454bbb24876cc22962f18
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2025-10-09 22:12:36 +0800

    Signal<T> to ReadonlySignal<T>

---
+++ Runtime/EventSignal/SignalExtension.cs
@@ -0,0 +1,7 @@
+﻿namespace AceLand.EventDriven.EventSignal
+{
+    public static class SignalExtension
+    {
+        public static ReadonlySignal<T> AsReadonly<T>(this Signal<T> signal) => signal;
+    }
+}
